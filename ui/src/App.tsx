aff0a79f36fd090298b599d1d807f32b122e6b00
Divi-1910 <dvmuley10@gmail.com>
2025-11-28 23:11:36 +0530

    boilerplate ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
